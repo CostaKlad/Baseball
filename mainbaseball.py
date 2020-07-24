3f1bdcc9184d1fab21c7903175b31734270b1e09
maniac73 <63009208+maniac73@users.noreply.github.com>
2020-07-24 19:54:48 -0400

    Add files via upload

--- mainbaseball.py
+++ mainbaseball.py
@@ -6,7 +6,7 @@ import pandas as pd
 df = pd.read_csv('Batting.csv')
 # df = pd.Dataframe(data)
 df['1B'] = df['H'] - df['HR'] - df['3B'] - df['2B']
-df['TrueHit'] = ((df['HR'] * 5) + (df['3B'] * 3) + (df['2B'] * 2) + (df['1B'] * 2) + df['BB'] -
+df['TrueHit'] = ((df['HR'] * 5) + (df['3B'] * 4) + (df['2B'] * 3) + (df['1B'] * 2) + df['BB'] -
                  df['SO']) / df['AB']
 df.to_csv('Truehit.csv')
 
